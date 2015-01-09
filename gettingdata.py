5e0ca947cbc4f6a9b4593f3ac5d3c0f04e0ce494
snehasishmohanta <snehasishmohanta@gmail.com>
2015-01-09 14:36:46 +0530

    Update gettingdata.py
    as it contain alot file inside the log file. so we need to findout all the data of time stand, initial query and number of sugessions.
    to get these data we need to run the above code named "gettingdata.py" by passing argument the log file named "nginx.log.2014-12-17-00" like this.
    >>>python gettingdata.py "nginx.log.2014-12-17-00"
    we will get all the data of our requirement.
    
    
    to store all the data inside a text file we need to run below code.
    
    >>>python gettingdata.py "nginx.log.2014-12-17-00" >temp.txt

--- gettingdata.py
+++ gettingdata.py
@@ -1,3 +1,24 @@
+# below is the log file look alike
+# and this file saved in "nginx.log.2014-12-17-00"
+
+"""
+[Dec 17 00:00:00] [27075] [shopclues-suggest] [info] -  Initial query: stives
+[Dec 17 00:00:00] [27075] [shopclues-suggest] [info] -  Input query: stives, Spell-corrected query: stives, NumSuggestions: 0, Time taken: 110 microseconds
+[Dec 17 00:00:04] [27075] [shopclues-suggest] [info] -  Initial query: west<U+200B><U+200B><U+200B>jackets
+[Dec 17 00:00:04] [27075] [shopclues-suggest] [info] -  Input query: west<U+200B><U+200B><U+200B>jackets, Spell-corrected query: west <U+200B>
+<U+200B><U+200B> jackets, NumSuggestions: 0, Time taken: 88 microseconds
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Initial query: <U+200B><U+200B><U+200B>combo offers 
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Input query: <U+200B><U+200B><U+200B>combo, Spell-corrected query: <U+200B><U+200B>
+<U+200B> combo offers, NumSuggestions: 10, Time taken: 164 microseconds
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Initial query: <U+200B><U+200B><U+200B>combo offer
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Input query: <U+200B><U+200B><U+200B>combo, Spell-corrected query: <U+200B><U+200B>
+<U+200B> combo offer, NumSuggestions: 10, Time taken: 126 microseconds
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Initial query: <U+200B><U+200B><U+200B>comb
+[Dec 17 00:00:05] [27075] [shopclues-suggest] [info] -  Input query: <U+200B><U+200B><U+200B>comb, Spell-corrected query: <U+200B><U+200B>
+<U+200B> comb, NumSuggestions: 10, Time taken: 58 microseconds
+"""
+# below is code to find out the  requrie data
+
 import re         #importing regularexpression
 import sys        
 file1 = sys.argv[1]  #here creating the environment to pass the argument the logfile
